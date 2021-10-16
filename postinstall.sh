3b2ca890575a692c52119dff831129277a31158c
und3rdg <und3rdg@gmail.com>
2021-10-16 20:26:14 +0100

    up

--- postinstall.sh
+++ postinstall.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 
-pacman -Sy git sudo ansible zsh
+pacman -Sy git sudo ansible zsh base-devel
 
+groupadd sudo
 useradd -m -G wheel sudo -s /bin/zsh undg
 
 echo "%sudo ALL=(ALL) ALL" >> /etc/sudoers
 
 su undg
 
+echo 'Hello undg, do you want me to setup your favorite environment for you?'
+read
+
 cd ~
 
 git clone https://github.com/undg/_init
 cd _init
 
+wait
 ansible-playbook local.yml -K
