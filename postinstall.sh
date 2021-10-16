a8614f4af9a7e24dd27cd05197a41eb29e3781ff
und3rdg <und3rdg@gmail.com>
2021-10-16 03:40:12 +0100

    pre ansible system setup and run ansible playbook.

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-pacman -Suy git sudo ansible zsh
+pacman -Sy git sudo ansible zsh
 
 useradd -m -G wheel sudo -s /bin/zsh undg
+
+echo "%sudo ALL=(ALL) ALL" >> /etc/sudoers
+
+su undg
+
+cd ~
+
+git clone https://github.com/undg/_init
+cd _init
+
+ansible-playbook local.yml -K
